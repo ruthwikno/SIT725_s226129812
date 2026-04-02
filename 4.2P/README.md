4ebbef165c3f09c2ea37baa7cf5014c19f8ca9fa
ruthwikno <ruthwikreddy.3711@gmial.com>
2026-04-03 00:43:21 +1100

    added 4.2P folder with the code of 3.2P

---
+++ 4.2P/README.md
@@ -0,0 +1,27 @@
+# SIT725 Assignment 3.2P: Supercar Show built using Node.js, Express, and Materialize
+SIT725 assignment 3.2P built using Node.js, Express, and Materialize. It’s a simple supercar gallery where I tried to implement dynamic UI, responsive layout, and interactive components.
+
+## Project Structure
+
+- `server.js` - Express server with static file serving from `public`.
+- `package.json` - project metadata and scripts.
+- `public/`:
+  - `index.html` - main page for the supercar app.
+  - `css/styles.css` - custom styles and responsive adjustments.
+  - `js/scripts.js` - car cards data array and rendering logic.
+  - `images/` - images used in cards.
+
+## How to run
+
+1. Run `npm install` (only dependency is `express`).
+2. Run `npm start` to start express server.
+3. Open `http://localhost:3000` in browser.
+
+## Limitations:
+
+- No backend
+- Modal form just prints data to console.
+- `About this car` links are placeholders (`href="#"`).
+
+
+
---
+++ 4.2P/public/js/scripts.js
@@ -0,0 +1,47 @@
+
+    const cardList = [
+{
+  title: "Koenigsegg Regera",
+  image: "images/koenigsegg.jpg",
+  link: "About Koenigsegg Regera",
+  description: "Top speed : 410 km/h\n0-100km/h : 2.8 sec\nEngine : 5.0L twin-turbo V8\nhorsepower : 1500 hp"
+},
+{
+  title: "Bugatti Chiron",
+  image: "images/Chiron.jpg",
+  link: "About Bugatti Chiron",
+  description: "Top speed : 420 km/h\n0-100km/h : 2.4 sec\nEngine : 8.0L W16 quad-turbocharged\nHorsepower : 1479 hp"
+}
+]
+const clickMe = () => {
+alert("Thanks for clicking me. Hope you have a nice day!")
+}
+const submitForm = () => {
+let formData = {};
+formData.first_name = $('#first_name').val();
+formData.last_name = $('#last_name').val();
+formData.password = $('#password').val();
+formData.email = $('#email').val();
+console.log("Form Data Submitted: ", formData);
+}
+const addCards = (items) => {
+items.forEach(item => {
+let itemToAppend = '<div class="col s4 center-align">'+
+'<div class="card medium"><div class="card-image waves-effect waves-block waves-light"><img class="activator" src="'+item.image+'">'+
+'</div><div class="card-content">'+
+'<span class="card-title activator grey-text text-darken-4">'+item.title+'<i class="material-icons right">more_vert</i></span><p><a href="#">'+item.link+'</a></p></div>'+
+'<div class="card-reveal">'+
+'<span class="card-title grey-text text-darken-4">'+item.title+'<i class="material-icons right">close</i></span>'+
+'<p class="card-text">'+item.description.replace(/\n/g,'<br>')+'</p>'+
+'</div></div></div>';
+$("#card-section").append(itemToAppend)
+});
+}
+$(document).ready(function(){
+$('.materialboxed').materialbox();
+$('#formSubmit').click(()=>{
+submitForm();
+})
+addCards(cardList);
+$('.modal').modal();
+});
